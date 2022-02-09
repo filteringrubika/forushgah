f64bbfebbe963634ef8628eb07f526806f2daa9a
filteringrubika <99149821+filteringrubika@users.noreply.github.com>
2022-02-09 04:13:57 +0330

    Create forushgah.py

---
+++ forushgah.py
@@ -0,0 +1,148 @@
+print("")
+print("")
+print("     . . . . . . . . . . . . . . . . . . . . . . . . .")
+print (" ")
+print("Load . . .")                 
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+print (Fore.YELLOW + "")
+print (""" Supermarket code reporter in virtual by _>ehsan """)
+print (Fore.GREEN + "")
+x = """ Loading ~ ~ ~ im ehsan """
+print ("|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>")
+print(f"{red} ")
+d = """ go sayte               Lodig . . . """
+print ("Linke: . . . . . . = https://ehsanlucien.mixin.ir . . . . . .♡")
+print(f"{green} ")
+print (Fore.WHITE + "")
+print ("Note that this expression is currently old")
+print (" ")
+#date
+x = str(datetime.datetime.now())
+
+print("Beginning " + (x))
+
+print(f"{blue} ")
+print ("         GOD FILTERING")
+print (" saytes jeme  ")
+print ("""The best filtering rule in virtual: (law-ryson.blogfa.com There are Ryson!
+rules.""")
+print(f"{blue} ")
+print ("      Linke HTTPS://EHSANLUCIEN.MIXIN.IR      ")
+print(f"{green} ")
+print (" khrid va froshe jem va dastorat termux keraCk . . . ")
+print (" . . . . . . . . . . . . . . . . . . . . . . . . .")
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style 
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+print ("Note that this expression is currently old")
+print (" ")
+#date
+x = str(datetime.datetime.now())
+
+print("Beginning " + (x))
+
+print(f"{blue} ")
+print ("         GOD FILTERING")
+print ("   They are the only creators of (Amir) ehsan ")
+print ("""The best filtering rule in virtual: (law-ryson.blogfa.com There are Ryson! rules.""")
+print (Fore.YELLOW + "")
+print (""" . . . . .  """)
+print (Fore.GREEN + "")
+x = """ Loading ~ ~ ~     . . . . . . . """
+# 1 chanel 2 Group 3 acc
+up = input(""" 
+[bastan (0) ]
+[linke frushgah   (1) ]
+[linke dorbin     (2) ]
+                 
+[eksperit  (3) ]
+Which do you want>>> """)
+print("")
+print(f"{pink} ")
+time.sleep(3.0)
+print(""" in the name of God 
+_____________
+ _  _
+ 
+💣 - 💣
+                      Loding....
+    
+||Web: creator-rayson.blogfa.com||
+|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|
+________rubika.ir/Caetorr________
+""")
+print(f"{red} ")
+
+if up == "0":
+         print ('(Super) Code {filter} =>' + '%s%s sms  %s' % (fg('red'), bg('black'), attr('reset')))
+
+
+if up == "1":
+
+        print ('code {Filter} (channel) ≠>' + '%s%s |>|https://ehsanlucien.mixin.ir|<| %s' % (fg('red'), bg('green'), attr('reset')))
+
+
+if up != "1":
+        print("   1.2.3.4.5  ")
+
+
+if up == "2":
+        print ('code {Filter} (group) ≠>' + '%s%s http://5555.2.1.8 passford|>|《admin》 %s' % (fg('white'), bg('yellow'), attr('reset')))
+
+
+if up != "2":
+        print(" https://ehsanlucien.mixin.ir ")
+       
+       
+if up == "3":
+
+        print ('code {Filter} (user) ≠>' + '%s%s http://5.7.2.1.lkian.u.w.q.y.i.e.d.a.fil.0193.com %s' % (fg('red'), bg('blue'), attr('reset')))
+
+
+if up != "3":
+        print(""" ehsan lucien""")
+print("  ")
+print (" ")
+print(f"{red} ")
+print ("     https://ehsanlucifer.mixin.ir    ")
